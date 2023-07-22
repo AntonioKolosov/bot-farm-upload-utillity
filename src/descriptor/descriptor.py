c310fb499b0f549e80c56d599390b154cfbb20fd
Ilya <bugatti141@gmail.com>
2023-07-22 20:58:46 +0300

    DynamoDB uploader added

--- src/descriptor/descriptor.py
+++ src/descriptor/descriptor.py
@@ -5,7 +5,7 @@ Descriptor
 
 class Descriptor:
     def __init__(self, bot_name) -> None:
-        self.output_root = "../new-topics"
+        self.output_root = "src\\new-bots"
         self.md_folder = "datatopics"
         self.cn_folder = "simple"
         self.bot_name = bot_name
--- src/uploader/uploader.py
+++ src/uploader/uploader.py
@@ -6,11 +6,31 @@ import os
 class AWS:
     def __init__(self):
         load_dotenv()
-        self.__session: boto3.Session = boto3.Session(
-            aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-            region_name=os.environ.get("AWS_DEFAULT_REGION"),
-        )
+        self.__session: boto3.Session = self.__create_boto3_session()
+
+    @staticmethod
+    def __create_boto3_session():
+        # Проверка наличия переменных окружения
+        required_env_vars = ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_DEFAULT_REGION"]
+        missing_vars = [var for var in required_env_vars if not os.environ.get(var)]
+        if missing_vars:
+            raise EnvironmentError(f"Missing required environment variables: {', '.join(missing_vars)}")
+
+        # Базовая валидация переменных окружения
+        # Просто проверяем, что они не пустые, но можно подумать перед деплоем над более строгой валидацией.
+        for var in required_env_vars:
+            if not os.environ.get(var).strip():
+                raise ValueError(f"Environment variable {var} cannot be empty.")
+
+        try:
+            session = boto3.Session(
+                aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+                aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+                region_name=os.environ["AWS_DEFAULT_REGION"],
+            )
+            return session
+        except Exception as e:
+            raise RuntimeError(f"Failed to create AWS session: {e}")
 
     @property
     def session(self):
